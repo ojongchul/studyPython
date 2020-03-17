425b3147f82ae0c324f8506a794b7f245be5e56d
ojongchul <ojongchul@gmail.com>
2020-03-17 22:16:59 +0900

    A  index.py A  number.py A  string.py

---
+++ string.py
@@ -0,0 +1,16 @@
+print ("help me")
+print ('help me' + " " + 'dohnald')
+print ("help 'me'")
+print ('help "me"')
+
+a = 'help me'
+print (a)
+print (len(a))
+print (a[0])
+print (a[1])
+print (a[2])
+print ((a+'\n')*3)
+print ('dohnald says ' + a )
+
+print ('{} says {}.'.format( 'dohnald', a))
+print (' {name} says {says}'.format(name='dohnald', says=a))
